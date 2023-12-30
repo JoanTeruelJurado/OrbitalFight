10e83b288036de3d2fe250eec8e0e0143f4a5017
christianrm02 <christianrm02@gmail.com>
2023-12-30 19:48:31 +0100

    fixed pinchos collider + portal bug + light protal bug

--- Assets/Scripts/lightPortal.cs
+++ Assets/Scripts/lightPortal.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class lightPortal : MonoBehaviour
 {
     private float alturaMaxima;
+    private float alturaMinima;
     private float velocidadAscenso = 1.2f;
+    private bool subiendo = true;
 
     private void Start() {
         alturaMaxima = transform.position.y+0.8f;
+        alturaMinima = transform.position.y;
     }
 
     private void Update()
     {
-        // Calcula la posición vertical utilizando Mathf.PingPong para oscilar entre 0 y alturaMaxima
-        float nuevaAltura = Mathf.PingPong(Time.time * velocidadAscenso, alturaMaxima);
+        if(transform.position.y >= alturaMaxima) subiendo = false;
+        else if(transform.position.y <= alturaMinima) subiendo = true;
 
-        // Establece la posición del objeto
-        transform.position = new Vector3(transform.position.x, nuevaAltura, transform.position.z);
+        if(subiendo) {
+            float nuevaAltura = transform.position.y + Time.deltaTime * velocidadAscenso;
+            transform.position = new Vector3(transform.position.x, nuevaAltura, transform.position.z);
+        }
+        else {
+            float nuevaAltura = transform.position.y - Time.deltaTime * velocidadAscenso;
+            transform.position = new Vector3(transform.position.x, nuevaAltura, transform.position.z);
+        }
+        // // Calcula la posición vertical utilizando Mathf.PingPong para oscilar entre 0 y alturaMaxima
+        // float nuevaAltura = Mathf.PingPong(Time.time * velocidadAscenso, alturaMaxima);
+
+        // // Establece la posición del objeto
+        // transform.position = new Vector3(transform.position.x, nuevaAltura, transform.position.z);
     }
 }
